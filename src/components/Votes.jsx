8b18f08277a16f3296dd1d5a1943c42dc2af386b
Alistair Sinclair-Smith <ajss90@hotmail.co.uk>
2022-05-17 11:38:52 +0100

    adding a votes component to display and change vote count on an article page

---
+++ src/components/Votes.jsx
@@ -0,0 +1,51 @@
+import { useState } from "react";
+import { FaThumbsUp, FaThumbsDown, FaUndoAlt } from "react-icons/fa";
+import {patchArticleById} from "../utils/api"
+
+const Votes = ({article, setError}) => {
+
+    const [voteCount, setVoteCount] = useState(article.votes);
+
+    const [resetValue, setResetValue] = useState(0);
+
+    const [disable, setDisable] = useState(false);
+
+    const updateVote = (votes, num) => {
+        return votes += num;
+    }
+
+    const handleVote = (incNum) => {
+        patchArticleById(article.article_id, {"inc_votes": incNum})
+            .catch((err) => {
+                setError({ err });
+            })
+    }
+    
+
+    return <div id="votes">
+        <p>{voteCount} votes</p>
+        <button aria-label="thumbs up" disabled={disable} onClick={(e) => {
+            setVoteCount((currVotes) => updateVote(currVotes, 1))
+            setResetValue(-1)
+            handleVote(1)
+            setDisable(true)
+            }}><FaThumbsUp/></button>
+        <button aria-label="thumbs down" disabled={disable} onClick={(e) => {
+            setVoteCount((currVotes) => updateVote(currVotes, -1))
+            setResetValue(1)
+            handleVote(-1)
+            setDisable(true)
+            }}><FaThumbsDown/></button>
+        {disable === true
+         ? 
+         <button aria-label="reset" onClick={(e) => {
+            setVoteCount((currVotes) => updateVote(currVotes, resetValue))
+            handleVote(resetValue)
+            setDisable(false)
+        }}><FaUndoAlt/></button>
+         : 
+         <></>}
+    </div>
+}
+
+export default Votes;
