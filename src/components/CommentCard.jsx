5a483036c3d0a1f71bc1be7c57057b9b6cce5308
Alistair Sinclair-Smith <ajss90@hotmail.co.uk>
2022-05-19 21:41:08 +0100

    amending for dynamic class names of comment card to be used with optimistic rendering

--- src/components/CommentCard.jsx
+++ src/components/CommentCard.jsx
@@ -3,7 +3,7 @@ import { MdDelete } from "react-icons/md";
 const CommentCard = ({commentData}) => {
 
     return (
-        <div className="comment-card">
+        <div className={commentData.comment_id === "newComment" ? "optimistic-comment-card" : "comment-card"}>
             <div className="comment-content">
                 <p className="comment-author"><strong>{commentData.author}</strong>: </p>
                 <p className="comment-body">{commentData.body}</p>
