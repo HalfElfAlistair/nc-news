53e88820a4fc5e28fa9766362b1a3263c633a0da
Alistair Sinclair-Smith <ajss90@hotmail.co.uk>
2022-05-20 20:16:53 +0100

    adding article id data to the ArticleCard component so the user can see that their sort query has been successful

--- src/components/ArticleCard.jsx
+++ src/components/ArticleCard.jsx
@@ -5,9 +5,14 @@ const ArticleCard = ({ articleData }) => {
  
     return <li className="article-card">
             <h3>{articleData.title}</h3>
-            <p>By: {articleData.author}</p>
-            <p>On: {dateExtract(articleData.created_at)}</p>
-            <div id="article-card-bottom">
+            <div className="article-card-top">
+                <div className="by-on-area">
+                    <p>By: {articleData.author}</p>
+                    <p>On: {dateExtract(articleData.created_at)}</p>
+                </div>
+                    <p className="article-id">#{articleData.article_id}</p>
+            </div>
+            <div className="article-card-bottom">
                 <p>Votes: {articleData.votes}</p>
                 <Link to={`/articles/${articleData.article_id}`}><button>Read More</button></Link>
             </div>
