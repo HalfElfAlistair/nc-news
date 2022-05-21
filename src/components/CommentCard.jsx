a251655196f41164409cf78603198e0ffaef9c47
Alistair Sinclair-Smith <ajss90@hotmail.co.uk>
2022-05-21 08:44:31 +0100

    adding optimistic rendering to button event which removes comment render

--- src/components/CommentCard.jsx
+++ src/components/CommentCard.jsx
@@ -1,16 +1,36 @@
 import { MdDelete } from "react-icons/md";
+import { useState } from "react";
+import { deleteComment } from "../utils/api";
 
-const CommentCard = ({commentData, currentUser}) => {
+const CommentCard = ({commentData, currentUser, setError}) => {
 
-    return (
+    const [deleteStatus, setDeleteStatus] = useState("active");
+
+    const deleteCheck = () => {
+        setDeleteStatus("deleted")
+        deleteComment(commentData.comment_id)
+            .catch((err) => setError({ err }))
+    }
+
+    return deleteStatus === "active" 
+    ? 
+    (
         <div className={commentData.comment_id === "newComment" ? "optimistic-comment-card" : "comment-card"}>
             <div className="comment-content">
                 <p className="comment-author"><strong>{commentData.author}</strong>: </p>
                 <p className="comment-body">{commentData.body}</p>
             </div>
-            {currentUser === commentData.author ? <button aria-label="delete"><MdDelete/></button> : <></>}
+            {currentUser === commentData.author 
+            ? <button aria-label="delete" onClick={(e) => deleteCheck()} ><MdDelete/></button> 
+            : <></>}
         </div>
-    )   
+    )
+    :
+    (
+        <div className="comment-card">
+            <p>Comment deleted.</p>
+        </div>
+    )
 }
 
 export default CommentCard;
