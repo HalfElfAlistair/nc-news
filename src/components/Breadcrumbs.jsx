a7e85cad75bec63c3024340e612a578ecb22780d
Alistair Sinclair-Smith <ajss90@hotmail.co.uk>
2022-05-24 22:10:40 +0100

    changing breadcrumbs so the current page crumb doesn't link

--- src/components/Breadcrumbs.jsx
+++ src/components/Breadcrumbs.jsx
@@ -1,23 +1,34 @@
 import { Link, useLocation } from 'react-router-dom';
 import { capitalise, topicCheck } from '../utils/helpers';
 
-const Breadcrumbs = ({setTopicSlug, topicSlug, topicsList}) => {
+const Breadcrumbs = ({setTopicSlug, topicSlug, topicsList, error}) => {
         
     const topicsArray = topicsList.map(topicObj => topicObj.slug)
 
     const location = useLocation();
     const crumbs = location.pathname.split("/").slice(1);
 
-    return <nav id="breadcrumbs">
-        <p><Link to="/" onClick={(e) => setTopicSlug("")}>Home</Link></p>
-        {crumbs.map((crumb) => {
-            if (crumb !== "articles") {
-                return <p key={crumb}> &gt; <Link to={"/" + crumb}>{capitalise(crumb)}</Link></p>
-            } else {
-                return topicCheck(topicSlug, topicsArray)
-            }
-        })}
-    </nav>;
+    return error 
+    ? 
+    <nav id="breadcrumbs">
+        <p><Link to="/" onClick={(e) => setTopicSlug("")}>Home &gt;</Link></p>
+    </nav>
+    : 
+    (
+        <nav id="breadcrumbs">
+            <p><Link to="/" onClick={(e) => setTopicSlug("")}>Home</Link></p>
+            {crumbs.map((crumb, index) => {
+                if (crumb !== "articles") {
+                    return index === crumbs.length-1 
+                    ? <p key={crumb}> &gt; {capitalise(crumb)}</p> 
+                    : 
+                    <p key={crumb}> &gt; <Link to={"/" + crumb}>{capitalise(crumb)}</Link></p>
+                } else {
+                    return topicCheck(topicSlug, topicsArray)
+                }
+            })}
+        </nav>
+    )
 }
 
 export default Breadcrumbs;
